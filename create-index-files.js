e6aea239b0c4fde91664a69965c4bedc423bfdfa
Niklas Lampén <niklas@lampen.fi>
2025-06-09 11:34:12 +0300

    Updates to structure

--- create-index-files.js
+++ create-index-files.js
@@ -113,21 +113,6 @@ console.log('\n📝 Creating main src/index.ts file...');
 
 const mainIndexContent = `import 'reflect-metadata';
 
-// Core decorators
-export * from './decorators';
-
-// OCPI Modules
-export * from './modules/locations';
-export * from './modules/tariffs';
-export * from './modules/sessions';
-export * from './modules/cdrs';
-export * from './modules/tokens';
-export * from './modules/commands';
-export * from './modules/chargingprofiles';
-export * from './modules/credentials';
-export * from './modules/versions';
-export * from './modules/hubclientinfo';
-
 // Common DTOs (root level)
 export * from './dtos/display-text.dto';
 export * from './dtos/price.dto';
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
 import 'reflect-metadata';
 
-// Core decorators
-export * from './decorators';
-
-// OCPI Modules
-export * from './modules/locations';
-export * from './modules/tariffs';
-export * from './modules/sessions';
-export * from './modules/cdrs';
-export * from './modules/tokens';
-export * from './modules/commands';
-export * from './modules/chargingprofiles';
-export * from './modules/credentials';
-export * from './modules/versions';
-export * from './modules/hubclientinfo';
-
 // Common DTOs (root level)
 export * from './dtos/display-text.dto';
 export * from './dtos/price.dto';
